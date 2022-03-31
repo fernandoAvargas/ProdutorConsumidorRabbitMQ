4ed174e5e2c11d66bcb64cb4f2c8429e4b2006d8
fernandoAvargas <fernando.avargas.fv@gmail.com>
2022-03-31 01:16:09 -0300

    Primeiro Commit

---
+++ RabbitMQ-Consumer/RabbitMQ-Consumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RabbitMQ_Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("****Receive Messages*****");
+
+            var rabbitMqClient = new RabbitMqClient();
+            rabbitMqClient.Listen();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RabbitMQ-Processor/RabbitMQ-Processor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RabbitMQ_Processor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("****Send Messages*****");
+            var rabbitMqClient = new RabbitMqClient();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                var message = string.Concat("Olá Rodolfo ", i, " ", DateTime.Now);
+
+                rabbitMqClient.Publish(message);
+
+                Console.WriteLine("Send Message = {0}", message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
